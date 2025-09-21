e521702d4e0dee5a43535104f53a19d097b0c224
goldenvertex42 <fabled93@gmail.com>
2025-09-21 13:25:42 -0500

    Create displayController module and update other modules to work with it

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const gameBoard = (() => {
 
     const getBoard = () => board;
 
-    const placeMarker = (index, marker) => {
+    const updateBoard = (index, marker) => {
         if (board[index] === "") {
             board[index] = marker;
             return true; // Move was successful
@@ -16,7 +16,7 @@ const gameBoard = (() => {
         board = ["", "", "", "", "", "", "", "", ""];
     };
 
-    return { getBoard, placeMarker, resetBoard };
+    return { getBoard, updateBoard, resetBoard };
 })();
 
 // GameController Module
@@ -27,12 +27,11 @@ const gameController = (() => {
     const player1 = player("Player 1", "X");
     const player2 = player("Player 2", "O");
     let currentPlayer = player1;
-    let gameOver = false;
 
     const switchPlayer = () => {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
     };
-    
+
     const checkWin = () => {
         const board = gameBoard.getBoard();
         const winPatterns = [
@@ -53,53 +52,85 @@ const gameController = (() => {
         }
     }
     return false;
-  };
+    };
 
-  const checkDraw = () => {
-    return gameBoard.getBoard().every((cell) => cell !== "");
-  };
+    const checkDraw = () => {
+        return gameBoard.getBoard().every((cell) => cell !== "");
+    };
 
-  const printBoard = () => {
-    const board = gameBoard.getBoard();
-    console.log(`
-      ${board[0] || "1"} | ${board[1] || "2"} | ${board[2] || "3"}
-      ---------
-      ${board[3] || "4"} | ${board[4] || "5"} | ${board[5] || "6"}
-      ---------
-      ${board[6] || "7"} | ${board[7] || "8"} | ${board[8] || "9"}
-    `);
-  };
+    const handlePlayerMove = (index) => {
+        if (gameBoard.updateBoard(index, currentPlayer.marker)) {
+            displayController.render();
+            if (checkWin(gameBoard.getBoard())) {
+                displayController.setMessageHandler(`${currentPlayer.name} wins!`);
+                displayController.addSquareClickHandler(() => {}); // Disable clicks
+            } else if (checkDraw(gameBoard.getBoard())) {
+                displayController.setMessageHandler("It's a tie!");
+                displayController.addSquareClickHandler(() => {}); // Disable clicks
+            } else {
+                switchPlayer();
+                displayController.setMessageHandler(`${currentPlayer.name}'s turn`);
+            }
+        }
+    };
 
-  const playTurn = (index) => {
-    if (gameOver) {
-      console.log("Game is over. Start a new game to play again.");
-      return;
-    }
+    const restartGame = () => {
+        gameBoard.resetBoard();
+        displayController.render();
+        currentPlayer = player1;
+        displayController.setMessageHandler(`${currentPlayer.name}'s turn`);
+        // Re-add the click handler to the board
+        displayController.addSquareClickHandler(handlePlayerMove);
+    };
 
-    if (gameBoard.placeMarker(index, currentPlayer.marker)) {
-      printBoard();
-      if (checkWin()) {
-        console.log(`${currentPlayer.name} wins!`);
-        gameOver = true;
-      } else if (checkDraw()) {
-        console.log("It's a draw!");
-        gameOver = true;
-      } else {
-        switchPlayer();
-        console.log(`${currentPlayer.name}'s turn (${currentPlayer.marker})`);
-      }
-    } else {
-      console.log("Invalid move. That spot is already taken or out of bounds.");
-    }
-  };
+    const init = () => {
+        displayController.render();
+        displayController.setMessageHandler(`${currentPlayer.name}'s turn`);
+        displayController.addSquareClickHandler(handlePlayerMove);
+        displayController.addRestartHandler(restartGame);
+    };
 
-  const startGame = () => {
-    gameBoard.resetBoard();
-    currentPlayer = player1;
-    gameOver = false;
-    printBoard();
-    console.log(`${currentPlayer.name}'s turn (${currentPlayer.marker})`);
-  };
+    return { init };
 
-  return { startGame, playTurn };
+
+  
 })();
+
+const displayController = (() => {
+    const boardElement = document.getElementById("game-board");
+    const messageElement = document.getElementById("message");
+    const restartButton = document.getElementById("restart-button");
+
+    const render = () => {
+        boardElement.innerHTML = "";
+        const board = gameBoard.getBoard();
+        board.forEach((marker, index) => {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.textContent = marker;
+            square.dataset.index = index; // Store the index in a data attribute
+            boardElement.appendChild(square);
+        });
+    };
+
+    const setMessageHandler = (message) => {
+        messageElement.textContent = message;
+    };
+
+    const addSquareClickHandler = (handler) => {
+        boardElement.addEventListener("click", (event) => {
+            if (event.target.classList.contains("square")) {
+                const index = event.target.dataset.index;
+                handler(index);
+            }
+        });
+    };
+
+    const addRestartHandler = (handler) => {
+        restartButton.addEventListener("click", handler);
+    };
+
+    return { render, setMessageHandler, addSquareClickHandler, addRestartHandler };
+})();
+
+gameController.init();
